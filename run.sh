c485dcad7cc3f639ca9fcd63a15fe01a51297d0b
mah54 <mrahimi262@gmail.com>
2021-03-10 07:47:13 +0000

    install nginx, php and mysql

--- run.sh
+++ run.sh
@@ -21,3 +21,9 @@ RIP=$(echo $IP | awk -F. '{print $4"."$3"."$2"."$1}') # Reverse IP for PTR recor
 
 ufw disable # is disable by default. Here I just make sure
 apt update
+
+yes | apt install nginx
+yes | apt install php-fpm php-mysql php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+systemctl restart php7.2-fpm
+
+yes | apt install mysql-server
